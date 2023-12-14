175c649741907f874263af04fa9b9266f506c99e
Roberto <roberto.quadraccia90@gmail.com>
2023-12-14 16:34:49 +0100

    Button and Input components created and styles moved

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { styled } from "styled-components";
 
+import Button from "./Button";
+import CustomInput from "./Input";
+
 // Styled in styled-components is an object where we can access the HTML elements.
 // The syntax below is called tagged template. We copy the style from CSS file inside ``.
 // It will give us a React component that automatically returns a <div>
@@ -15,34 +18,6 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => (props.invalid ? "#f87171" : "#6b7280")};
-`;
-
-// Above, we conditionally set the color by using a function that takes props (set inside the component below as invalid),
-// then accessing it as an object props.invalid.
-
-// IMPORTANT: These components, which we build with styled dot,
-// do not just use the children prop so that we can wrap them around content like this texture.
-// But in addition, they also forward all props we're setting on this styled component
-// to the underlying built-in JSX element. So the built-in <label> in this case here.
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -66,39 +41,30 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          {/* 
-          We can use the above dollar sign so that our invalid won't clash with the invalid built-in prop
-          */}
-          <Input
-            type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
-            // We can use inline style above to dynamically change the backgroundColor based on the emailNotValid condition
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
+        <CustomInput
+          type="email"
+          // style={{
+          //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+          // }}
+          // We can use inline style above to dynamically change the backgroundColor based on the emailNotValid condition
+          label="Email"
+          invalid={emailNotValid}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <CustomInput
+          type="password"
+          label="Password"
+          invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
