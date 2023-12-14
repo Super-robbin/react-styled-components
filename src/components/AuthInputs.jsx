87a5a4aeeb97708f592c9bbe9fb8f23ad14ff438
Roberto <roberto.quadraccia90@gmail.com>
2023-12-14 15:14:18 +0100

    Intro to styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+
+// Styled in styled-components is an object where we can access the HTML elements.
+// The syntax below is called tagged template. We copy the style from CSS file inside ``.
+// It will give us a React component that automatically returns a <div>
+// that has these styles applied to it. It will be a div that internally also uses the special children prop
+// so that it can be wrapped around other content. 
+// Finally, we replace the <div> below with the ControlContainer component.
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,14 +32,16 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
+          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
+            Email
+          </label>
           {/* 
           We can use the above approach to always have label, but also have invalid if emailNotValid is truthy
           */}
@@ -34,26 +51,30 @@ export default function AuthInputs() {
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
             // }}
             // We can use inline style above to dynamically change the backgroundColor based on the emailNotValid condition
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>
+            Password
+          </label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
